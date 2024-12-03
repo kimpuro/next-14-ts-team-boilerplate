ca2ac7e908fb7088a06512055afc12feac121008
KimPuro <puro@protonmail.ch>
2024-12-03 14:26:45 +0900

    chore: add some icons at cz-config

--- .cz-config.js
+++ .cz-config.js
@@ -1,28 +1,34 @@
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat:     새로운 기능의 추가, 기존 기능을 수정' },
-    { value: 'fix', name: 'fix:      버그 수정' },
-    { value: 'docs', name: 'docs:     문서 관련 수정' },
+    {
+      value: 'feat',
+      name: '✨  feat:     새로운 기능의 추가, 기존 기능을 수정',
+    },
+    { value: 'fix', name: '🐛 fix:      버그 수정' },
+    { value: 'docs', name: '📝 docs:     문서 관련 수정' },
     {
       value: 'style',
-      name: 'style:    코드의 의미에 영향을 미치지 않는 변경 사항(공백, 서식, 세미콜론 누락 등)',
+      name: '🎨 style:    코드의 의미에 영향을 미치지 않는 변경 사항(공백, 서식, 세미콜론 누락 등)',
     },
-    { value: 'refactor', name: 'refactor: 기능의 변화가 없는 코드 리팩터링' },
-    { value: 'perf', name: 'perf:     성능 향상을 위한 코드 변경' },
+    {
+      value: 'refactor',
+      name: '♻️ refactor: 기능의 변화가 없는 코드 리팩터링',
+    },
+    { value: 'perf', name: '⚡ perf:     성능 향상을 위한 코드 변경' },
     {
       value: 'test',
-      name: 'test:     테스트 코드 추가, 기존 테스트 코드 변경',
+      name: '✅  test:     테스트 코드 추가, 기존 테스트 코드 변경',
     },
     {
       value: 'build',
-      name: 'build:    빌드 시스템이나 외부 종속성에 영향을 미치는 변경 사항(예시 : gulp, broccoli, npm)',
+      name: '🔧 build:    빌드 시스템이나 외부 종속성에 영향을 미치는 변경 사항(예시 : gulp, broccoli, npm)',
     },
-    { value: 'ci', name: 'ci:       CI 구성 파일 및 스크립트 변경' },
+    { value: 'ci', name: '👷 ci:       CI 구성 파일 및 스크립트 변경' },
     {
       value: 'chore',
-      name: 'chore:    소스 또는 테스트 파일을 수정하지 않는 기타 변경 사항',
+      name: '🔨 chore:    소스 또는 테스트 파일을 수정하지 않는 기타 변경 사항',
     },
-    { value: 'revert', name: 'revert:   이전 커밋을 revert' },
+    { value: 'revert', name: '⏪  revert:   이전 커밋을 revert' },
   ],
   messages: {
     type: '커밋하려는 변경 사항의 유형을 선택하세요 :',
